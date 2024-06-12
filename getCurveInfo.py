91017ee2815c5140642e10c466c72adb84469f1a
ameliakratzer-code <akratzer@andrew.cmu.edu>
2024-06-12 10:36:51 -0700

    added plot

--- getCurveInfo.py
+++ getCurveInfo.py
@@ -1,10 +1,11 @@
 import pymysql
 import argparse
+import matplotlib.pyplot as plt
 
 # Handle command line arguments
-# By default, arguments are strings
 parser = argparse.ArgumentParser('Allow user to input site name, period')
 parser.add_argument('--sitename')
+parser.add_argument('--period')
 args = parser.parse_args()
 
 # Connect to the database 
@@ -25,20 +26,33 @@ with connection.cursor() as cursor:
     cursor.execute(query1)
     runID = cursor.fetchall()
     # Use query1 value - the run_Id as WHERE Hazard_Curves.Run_ID = query1
-    query2 = '''SELECT * FROM Hazard_Curve_Points
+    period = int(args.period)
+    query2 = f'''SELECT * FROM Hazard_Curve_Points
            INNER JOIN Hazard_Curves
            ON Hazard_Curve_Points.Hazard_Curve_ID = Hazard_Curves.Hazard_Curve_ID
            INNER JOIN IM_Types
            ON Hazard_Curves.IM_Type_ID = IM_Types.IM_Type_ID
-           WHERE Hazard_Curves.Run_ID = %s AND IM_Types.IM_Type_Value = 2 AND IM_Types.IM_Type_Component='RotD50'
+           WHERE Hazard_Curves.Run_ID = %s AND IM_Types.IM_Type_Value = {period} AND IM_Types.IM_Type_Component='RotD50'
            '''
     cursor.execute(query2, (runID))
     result = cursor.fetchall()
 
-# Print out result
+# plot of hazard curve using matplotlib
+plt.xscale('linear')
+plt.xlim(0, 2)
+plt.yscale('log')
+plt.ylim(1e-6,1)
+plt.xlabel('Accel (g)')
+plt.ylabel('Prob')
+plt.title('PAS Hazard Curve')
+#get list of x and y coordinates
+xCoords = []
+yCoords = []
 for row in result:
-    print(row)
-print(len(result))
+    xCoords.append(row[2])
+    yCoords.append(row[3])
+plt.plot(xCoords, yCoords, marker='^')
+plt.grid(axis = 'y')
+plt.show()
 
 connection.close()
-
