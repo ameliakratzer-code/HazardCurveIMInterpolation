20c4039b529fcc75812bbbf2b887bb5baf8ab23d
ameliakratzer-code <akratzer@andrew.cmu.edu>
2024-06-11 17:11:52 -0700

    connection to db

--- getCurveInfo.py
+++ getCurveInfo.py
@@ -1,1 +1,9 @@
-print('hello')
+import pymysql
+
+# Connect to the database 
+connection = pymysql.connect(host = 'localhost',
+                             user = 'cybershake_ro',
+                             password = 'CyberShake2007',
+                             database = 'moment.usc.edu')
+
+# 
