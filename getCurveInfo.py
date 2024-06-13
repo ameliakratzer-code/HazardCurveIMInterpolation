4ec16d910c4a3a38c0b7bc9e4183b44f9dfb0997
ameliakratzer-code <akratzer@andrew.cmu.edu>
2024-06-12 19:40:12 -0700

    save plot as image if name is provided

--- getCurveInfo.py
+++ getCurveInfo.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser('Allow user to input site name, period')
 parser.add_argument('--sitename')
 parser.add_argument('--period')
+parser.add_argument('--outputName')
 args = parser.parse_args()
 
 # Connect to the database 
@@ -16,26 +17,24 @@ connection = pymysql.connect(host = 'moment.usc.edu',
 
 with connection.cursor() as cursor:
     # Queries to get hazard curve information
-    query1 = f'''SELECT CyberShake_Runs.Run_ID FROM CyberShake_Sites
+    query1 = '''SELECT CyberShake_Runs.Run_ID FROM CyberShake_Sites
                 INNER JOIN CyberShake_Runs
                 ON CyberShake_Sites.CS_Site_ID = CyberShake_Runs.Site_ID
                 INNER JOIN Studies
                 ON CyberShake_Runs.Study_ID = Studies.Study_ID
-                WHERE CyberShake_Sites.CS_Short_Name = '{args.sitename}' AND Studies.Study_Name = 'Study 22.12 LF';
-    '''
-    cursor.execute(query1)
+                WHERE CyberShake_Sites.CS_Short_Name = %s AND Studies.Study_Name = 'Study 22.12 LF';
+                '''
+    cursor.execute(query1, (args.sitename))
     runID = cursor.fetchall()
     # Use query1 value - the run_Id as WHERE Hazard_Curves.Run_ID = query1
-    # Convert period to number
-    period = int(args.period) if args.period != '7.5' else float(args.period)
+    period = float(args.period)
     query2 = '''SELECT * FROM Hazard_Curve_Points
            INNER JOIN Hazard_Curves
            ON Hazard_Curve_Points.Hazard_Curve_ID = Hazard_Curves.Hazard_Curve_ID
            INNER JOIN IM_Types
            ON Hazard_Curves.IM_Type_ID = IM_Types.IM_Type_ID
-           WHERE Hazard_Curves.Run_ID = %s AND IM_Types.IM_Type_Value = %s AND IM_Types.IM_Type_Component='RotD50'
-           '''
-    cursor.execute(query2, (runID,period))
+           WHERE Hazard_Curves.Run_ID = %s AND IM_Types.IM_Type_Value = %s AND IM_Types.IM_Type_Component='RotD50';'''
+    cursor.execute(query2, (runID, period))
     result = cursor.fetchall()
 connection.close()
 
@@ -55,4 +54,9 @@ for row in result:
     yCoords.append(row[3])
 plt.plot(xCoords, yCoords, marker='^')
 plt.grid(axis = 'y')
-plt.show()
+
+# If outputName provided, save the plot to the given name
+if args.outputName != None:
+    plt.savefig(args.outputName + '.png')
+else:
+    plt.show()
