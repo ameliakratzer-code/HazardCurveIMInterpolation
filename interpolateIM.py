838ca841c301914374f3a2baf4a5430857fec130
ameliakratzer-code <akratzer@andrew.cmu.edu>
2024-06-20 16:43:33 -0700

    query to get IM values

--- interpolateIM.py
+++ interpolateIM.py
@@ -1,1 +1,31 @@
+import pymysql
+import argparse
+import matplotlib.pyplot as plt
 
+# Connect to the database
+connection = pymysql.connect(host = 'moment.usc.edu',
+                            user = 'cybershk_ro',
+                            password = 'CyberShake2007',
+                            database = 'CyberShake')
+
+def getIMValues(nameSite):
+    with connection.cursor() as cursor:
+        IMVals = '''
+        SELECT P.Source_ID, P.Rupture_ID, P.Rup_Var_ID, P.IM_Value 
+        FROM CyberShake_Sites S, CyberShake_Runs R, PeakAmplitudes P, Studies T, IM_Types I
+        WHERE S.CS_Short_Name = %s
+        AND S.CS_Site_ID = R.Site_ID
+        AND R.Study_ID = T.Study_ID
+        AND T.Study_Name = 'Study 22.12 LF'
+        AND R.Run_ID = P.Run_ID
+        AND I.IM_Type_Component = 'RotD50'
+        AND I.IM_Type_Value = 2.0
+        AND I.IM_Type_ID = P.IM_Type_ID;
+        '''
+        cursor.execute(IMVals, (nameSite))
+        result = cursor.fetchall()
+
+def main():
+    getIMValues('USC')
+
+main()
