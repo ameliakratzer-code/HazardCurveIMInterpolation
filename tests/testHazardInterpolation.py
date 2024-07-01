1506f1763f8158b6521a14f646b9e42a7106bb5d
ameliakratzer-code <akratzer@andrew.cmu.edu>
2024-07-01 14:23:49 -0700

    try

--- tests/testHazardInterpolation.py
+++ tests/testHazardInterpolation.py
@@ -5,27 +5,26 @@ sys.path.append('/home1/10000/ameliakratzer14/Pasadena')
 from getCurveInfo import bilinearinterpolation
 from unittest.mock import patch
 
-@patch('args', ['--sitenames', 'S385,S429,S431,S387', '--interpsitename', 'COO', '--output', '$SCRATCH'])
-@patch('argparse.ArgumentParser.parse_args')
 class testHazardCurveInterpolater(unittest.TestCase):
     # Testing accuracy of interpolation function
     def testInterpolationCalcs(self):
-        # Get ref results list
-        referenceFile = 'ReferenceCOO.csv'
-        refResultsL = []
-        with open(referenceFile, 'r') as file:
-            read = csv.reader(file)
-            # Skip headers, vals read as strings
-            next(read)
-            for row in read:
-                refResultsL.append(float(row[1]))
-        # Compare ref results list to current results
-        currentResultsL = bilinearinterpolation()
-        errorTolerance = 0.001 / 100
-        for i in range(len(refResultsL)):
-            difference = abs(refResultsL[i]-currentResultsL[i])
-            # Unittest method that checks whether difference is <= errorTolerance
-            self.assertLessEqual(difference, errorTolerance, 'Difference exceeds error tolerence')
+        with patch('sys.argv', ['python3', 'getCurveInfo.py', '--sitenames', 'S385,S429,S431,S387', '--interpsitename', 'COO', '--output', '$SCRATCH']):
+            # Get ref results list
+            referenceFile = 'ReferenceCOO.csv'
+            refResultsL = []
+            with open(referenceFile, 'r') as file:
+                read = csv.reader(file)
+                # Skip headers, vals read as strings
+                next(read)
+                for row in read:
+                    refResultsL.append(float(row[1]))
+            # Compare ref results list to current results
+            currentResultsL = bilinearinterpolation()
+            errorTolerance = 0.001 / 100
+            for i in range(len(refResultsL)):
+                difference = abs(refResultsL[i]-currentResultsL[i])
+                # Unittest method that checks whether difference is <= errorTolerance
+                self.assertLessEqual(difference, errorTolerance, 'Difference exceeds error tolerence')
 
 if __name__ == '__main__':
     unittest.main()
