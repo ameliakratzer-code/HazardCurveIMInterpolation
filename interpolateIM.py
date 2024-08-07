0aaa4a29aaddd4e18922f5090bb5893ce33fb3e8
ameliakratzer-code <akratzer@andrew.cmu.edu>
2024-08-06 20:45:17 -0700

    v

--- interpolateIM.py
+++ interpolateIM.py
@@ -129,10 +129,10 @@ def bilinearinterpolation(s0, s1, s2, s3, sI):
             write.writerow([event, IMVal])
     print('Data downloaded')
     # Scatterplot of data
-    # interpScatterplot(p4.valsToInterp, interpIMVals)
-    # print('Scatterplot plotted')
+    interpScatterplot(p4.valsToInterp, interpIMVals)
+    print('Scatterplot plotted')
     # Print out percent error and create a histogram for each event
-    """ listDifferences = []
+    listDifferences = []
     for i in range(len(interpIMVals)):
         # Percent difference = (interp-simulated) / simulated not absolute
         percentDifference = ((interpIMVals[i] - p4.valsToInterp[i]) / p4.valsToInterp[i]) * 100
@@ -147,10 +147,10 @@ def bilinearinterpolation(s0, s1, s2, s3, sI):
     filePath = args.output + 'histogram.png'
     plt.savefig(filePath)
     plt.close()
-    print('Histogram plotted') """
+    print('Histogram plotted')
     # Scatterplots by magnitude
     # Event = (source, rup, rupVar)
-    """ cursor = connection.cursor()
+    cursor = connection.cursor()
     index1, index2, index3, index4 = [], [], [], []
     bin1x, bin1y, bin2x, bin2y, bin3x, bin3y, bin4x, bin4y = [], [], [], [], [], [], [], []
     # Append index of the event instead of event itself since want to access IM val
@@ -197,7 +197,7 @@ def bilinearinterpolation(s0, s1, s2, s3, sI):
     plt.savefig(filePath)
     print('Magnitude subplot plotted')
     cursor.close()
- """
+
 def mag_scatter(ax, x, y, title):
     ax.scatter(x, y, color='blue', s=4)
     ax.set_title(title)
