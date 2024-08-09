b06682c3d2290ad74d5cf5fce342e7304fcfc8ab
ameliakratzer-code <akratzer@andrew.cmu.edu>
2024-08-09 16:27:19 -0700

    c

--- IMML2.py
+++ IMML2.py
@@ -9,26 +9,6 @@ from sklearn.model_selection import train_test_split
 import csv
 import joblib
 
-def makeScatterplot(ySimList, yPredictionList):
-    plt.scatter(ySimList, yPredictionList, color='blue')
-    plt.title('Simulated versus Interpolated Values')
-    plt.xlabel('Simulated')
-    plt.ylabel('Interpolated')
-
-    model = LinearRegression()
-    model.fit(ySimList, yPredictionList)
-    y_fit = model.predict(ySimList)
-    plt.plot(ySimList, y_fit, color='green', linestyle='-', label='Line of Best Fit')
-
-    x_limits = plt.gca().get_xlim()
-    y_limits = plt.gca().get_ylim()
-    min_val = min(x_limits[0], y_limits[0])
-    max_val = max(x_limits[1], y_limits[1])
-    plt.plot([min_val, max_val], [min_val, max_val], color='red', linestyle='--', label='y = x')
-    plt.legend()
-    plt.xlim(x_limits)
-    plt.ylim(y_limits)
-
 # Four command line arguments: input file name, name of folder, name of files, 1 if want to save
 data = joblib.load('preprocessed_data_and_scalers.pkl')
 X_train = data['X_train']
@@ -39,7 +19,8 @@ Xscaler = data['Xscaler']
 Yscaler = data['Yscaler']
 X_inference = data['X_inference']
 simVals = data['simVals']
-BATCH_SIZE = 128
+print(simVals)
+BATCH_SIZE = 64
 EPOCHS = 10
 INPUT_SIZE = 23
 OUTPUT_SIZE = 1
@@ -64,8 +45,9 @@ model.add(tf.keras.layers.Activation('softplus'))
 
 model.add(tf.keras.layers.Dense(OUTPUT_SIZE , activation='sigmoid')) 
 
-optimize = tf.keras.optimizers.Adam(learning_rate=0.0001)
+optimize = tf.keras.optimizers.Adam(learning_rate=0.001)
 model.compile(optimizer = optimize, loss='mean_squared_error')
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(X_test,y_test))
 
 score = model.evaluate(X_test,y_test,verbose=0)
@@ -87,6 +69,8 @@ if True:
     # Scatterplot
     yInferenceNorm = model.predict(X_inference)
     yInference = Yscaler.inverse_transform(yInferenceNorm.reshape(-1,1)).ravel()
+    print(yInference[:100])
+    print(simVals[:100])
     # Data to use for hazard curve calc
     fileName = f'USCIM.csv'
     filePath = sys.argv[2] + '/USCinference.csv'
@@ -97,7 +81,4 @@ if True:
             # Event number does not really matter
             write.writerow([f"(132, 39, {i})", IMVal])
             i += 1
-    plt.figure(2)
-    makeScatterplot(simVals, yInference)
-    plt.savefig(sys.argv[2] + '/USCinference.png')
 
